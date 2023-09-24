2a2908137de01e333727594e2f43823c2b4dcdb5
Fiquell <fiquellh@gmail.com>
2023-09-25 01:29:16 +0800

    Added trimmed whitespace

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,14 @@
 type Opts =
     | string
     | {
+          trim?: boolean;
           replacement?: string;
           lowercase?: boolean;
       };
 
 export default function slugizer(str: string, opts?: Opts) {
     const defaultOpts: Opts = {
+        trim: true,
         replacement: "-",
         lowercase: true,
     };
@@ -15,6 +17,10 @@ export default function slugizer(str: string, opts?: Opts) {
         opts = { ...defaultOpts, replacement: opts };
     } else {
         opts = { ...defaultOpts, ...opts };
+    }
+
+    if (opts.trim) {
+        str = str.trim();
     }
 
     let slug = str.replace(/[^a-zA-Z0-9]/g, opts.replacement as string);
