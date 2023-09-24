c5acfdad8a59577c44377a0c89fb79d914b66ac3
Fiquell <fiquellh@gmail.com>
2023-09-25 00:21:52 +0800

    Added default options

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,27 @@
-console.log("slugizer");
+type Opts =
+    | string
+    | {
+          replacement?: string;
+          lowercase?: boolean;
+      };
+
+export default function slugizer(str: string, opts?: Opts) {
+    const defaultOpts: Opts = {
+        replacement: "-",
+        lowercase: true,
+    };
+
+    if (typeof opts === "string") {
+        opts = { ...defaultOpts, replacement: opts };
+    } else {
+        opts = { ...defaultOpts, ...opts };
+    }
+
+    let slug = str.replace(/[^a-zA-Z0-9]/g, opts.replacement as string);
+
+    if (opts.lowercase) {
+        slug = slug.toLowerCase();
+    }
+
+    return slug;
+}
